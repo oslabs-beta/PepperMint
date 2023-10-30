9aae551d1980f5868d945b645ccde9552a1754a1
Nassim Dev <nassimdeveloper@Nassims-MacBook-Pro.local>
2023-10-30 11:13:55 -0400

    MVP functionality completed and refactored to remove unnecessary nesting in data structure

--- src/components/CreateTemplate.js
+++ src/components/CreateTemplate.js
@@ -18,10 +18,13 @@ const CreateTemplate = (props) => {
 
   const codeTemplate = [
     '\n', '\n', '\n', '\n', '\n', '\n', `describe((\'Unit Testing for${componentName}') => {`, '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n',
-    '\n', '\n', '\n', '\n', '\n', '});', '\n',
+    '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '});', '\n',
   ];
 
   const codeLineLib = {
+    // a: '\na {\n\n\n}\n',
+    // b: '\nb( )bb( )( )\n',
+    // c: '\nc {\n\n\n}\n'
     imports: '\nimport React from \'React\'\nimport { render, cleanup, screen, waitFor } from \'@testing-library/react\'\nimport userEvent from \'@testing-library/user-event\'\n',
     props: '\nprops {\n\n\n}\n',
     testblock: '\ntest((\' \') => {\n\n\n}\n',
@@ -32,6 +35,11 @@ const CreateTemplate = (props) => {
   }
 
   const insertionPtLib = {
+    // a: [{start: {line: 2, char: 0}, end: {line: 3, char: 0}, kind: 'codemirror-subinsert'}],
+    // b: [{start: {line: 1, char: 2}, end: {line: 1, char: 3}, kind: 'codemirror-inline'}, 
+    //     {start: {line: 1, char: 7}, end: {line: 1, char: 8}, kind: 'codemirror-inline'},
+    //     {start: {line: 1, char: 10}, end: {line: 1, char: 11}, kind: 'codemirror-inline'}],
+    // c: [{start: {line: 2, char: 0}, end: {line: 3, char: 0}, kind: 'codemirror-subinsert'}]
     props: [{start: {line: 2, char: 0}, end: {line: 3, char: 0}, kind: 'codemirror-subinsert'}],
     beforeAll: [{start: {line: 2, char: 0}, end: {line: 3, char: 0}, kind: 'codemirror-subinsert'}],
     newProp: [{start: {line: 2, char: 0}, end: {line: 3, char: 0}, kind: 'codemirror-subinsert'}],
@@ -42,30 +50,34 @@ const CreateTemplate = (props) => {
 
   class InsertionObject {
 
-    constructor(startPos, endPos, kind, tabIndex) {
+    constructor(startPos, endPos, kind, singleOrMultiple) {
       this.insertionStart = bookmarkCreator(startPos, kind);
       this.insertionEnd = bookmarkCreator(endPos, kind)
       this.kind = kind;
       this.contents = [];
-      this.tabIndex = tabIndex;
+      this.insertionPts = [{insertionStart: this.insertionStart, insertionEnd: this.insertionEnd}];
+      this.allowAdditional = true;
     }
 
     addContents(title, allowAdditional) {
-      doc.replaceRange(codeLineLib[title], this.fetchPosition('s'), this.fetchPosition('e'));
-      this.addInsertionPts(title, allowAdditional);
+
+      if (this.allowAdditional) {
+        doc.replaceRange(codeLineLib[title], this.fetchPosition('s'), this.fetchPosition('e'));
+        this.addInsertionPts(title, allowAdditional);
+      }
+
+      if (allowAdditional === false) this.allowAdditional = false;
     }
 
     addInsertionPts(title, allowAdditional) {
 
       if (!(insertionPtLib[title])) return;
 
-      const newInsertion = new InsertionObject(this.fetchPosition('s'), this.fetchPosition('e'), this.kind);
-      // const newInsertion = [];
-
+      const newInsertion = [];
 
       insertionPtLib[title].forEach((insertionPt) => {
   
-        newInsertion.contents.push(
+        newInsertion.push(
           new InsertionObject(
             {line: this.insertionStart.find().line + insertionPt.start.line, ch: insertionPt.start.char}, 
             {line: this.insertionStart.find().line + insertionPt.end.line, ch: insertionPt.end.char},
@@ -77,28 +89,20 @@ const CreateTemplate = (props) => {
 
       });
 
-      this.handleNewLines(this.kind, allowAdditional);
+      if (allowAdditional) this.handleNewLines(this.kind);
 
     }
 
-    handleNewLines(kind, allowAdditional) {
-
-      if (kind !== 'codemirror-inline' && allowAdditional === true) {
+    handleNewLines(kind) {
 
         const insertionEndPos = this.fetchPosition('e');
-        this.insertionStart.clear();
-        this.insertionEnd.clear();
   
         doc.replaceRange('\n\n', insertionEndPos)
-        this.insertionStart = bookmarkCreator({line: insertionEndPos.line + 1, ch: 0}, 'codemirror-insertion-point');
-        this.insertionEnd = bookmarkCreator({line: insertionEndPos.line + 2, ch: 0}, 'codemirror-insertion-point');  
+        this.insertionStart = bookmarkCreator({line: insertionEndPos.line + 1, ch: 0}, kind);
+        this.insertionEnd = bookmarkCreator({line: insertionEndPos.line + 2, ch: 0}, kind);
+        
+        this.insertionPts.push({insertionStart: this.insertionStart, insertionEnd: this.insertionEnd})
 
-      }
-
-      else if (kind !== 'codemirror-inline' && allowAdditional === false) {
-        this.insertionStart.clear();
-        this.insertionEnd.clear();     
-      }
 
     }
 
@@ -110,17 +114,17 @@ const CreateTemplate = (props) => {
 
     }
 
-    clearMarks() {
+    // clearMarks() {
 
-      this.insertionStart.clear();
-      this.insertionEnd.clear();
+    //   this.insertionStart.clear();
+    //   this.insertionEnd.clear();
 
-      this.contents.forEach((insertionObj) => {
-        this.insertionStart.clear();
-        this.insertionEnd.clear();
-      })
+    //   this.contents.forEach((insertionObj) => {
+    //     this.insertionStart.clear();
+    //     this.insertionEnd.clear();
+    //   });
 
-    }
+    // }
 
   }
 
@@ -133,45 +137,58 @@ const CreateTemplate = (props) => {
     Object.assign(
       macroStructure,
       {
+        // a: {
+        //   start: bookmarkCreator({line: 0, char: 0}),
+        //   end: bookmarkCreator({line: 3, char: 0}),
+        //   insertionZone: new InsertionObject({line: 1, char: 0}, {line: 2, char: 0}, 'codemirror-insertion-point')
+        // },
+        // b: {
+        //   start: bookmarkCreator({line: 6, char: 0}),
+        //   end: bookmarkCreator({line: 9, char: 0}),
+        //   insertionZone: new InsertionObject({line: 7, char: 0}, {line: 8, char: 0}, 'codemirror-insertion-point')  
+        // },
+        // c: {
+        //   start: bookmarkCreator({line: 12, char: 0}),
+        //   end: bookmarkCreator({line: 15, char: 0}),
+        //   insertionZone: new InsertionObject({line: 13, char: 0}, {line: 14, char: 0}, 'codemirror-insertion-point')
+        // }
         imports: {
-          indentLevel: 0,
           start: bookmarkCreator({line: 0, char: 0}),
           end: bookmarkCreator({line: 3, char: 0}),
           insertionZone: new InsertionObject({line: 1, char: 0}, {line: 2, char: 0}, 'codemirror-insertion-point')
         },
         props: {
-          indentLevel: 0,
           start: bookmarkCreator({line: 9, char: 0}),
           end: bookmarkCreator({line: 12, char: 0}),
           insertionZone: new InsertionObject({line: 10, char: 0}, {line: 11, char: 0}, 'codemirror-insertion-point')
         },
         mocks: {
-          indentLevel: 0,
-          start: bookmarkCreator({line: 14, char: 0}),
-          end: bookmarkCreator({line: 17, char: 0}),
-          insertionZone: new InsertionObject({line: 15, char: 0}, {line: 16, char: 0}, 'codemirror-insertion-point')
+          start: bookmarkCreator({line: 15, char: 0}),
+          end: bookmarkCreator({line: 18, char: 0}),
+          insertionZone: new InsertionObject({line: 16, char: 0}, {line: 17, char: 0}, 'codemirror-insertion-point')
         },
         jestHooks: {
-          indentLevel: 0,
-          start: bookmarkCreator({line: 19, char: 0}),
-          end: bookmarkCreator({line: 20, char: 0}),
-          insertionZone: new InsertionObject({line: 19, char: 0}, {line: 20, char: 0}, 'codemirror-insertion-point')
+          start: bookmarkCreator({line: 21, char: 0}),
+          end: bookmarkCreator({line: 24, char: 0}),
+          insertionZone: new InsertionObject({line: 22, char: 0}, {line: 23, char: 0}, 'codemirror-insertion-point')
         },
         testZone: {
-          indentLevel: 0,
-          start: bookmarkCreator({line: 23, char: 0}),
-          end: bookmarkCreator({line: 26, char: 0}),
-          insertionZone: new InsertionObject({line: 24, char: 0}, {line: 25, char: 0}, 'codemirror-insertion-point')
+          start: bookmarkCreator({line: 27, char: 0}),
+          end: bookmarkCreator({line: 30, char: 0}),
+          insertionZone: new InsertionObject({line: 28, char: 0}, {line: 29, char: 0}, 'codemirror-insertion-point')
         },
       }
     );
     
+    // // macroStructure['a'].insertionZone.addContents('a', false);
+    // macroStructure['b'].insertionZone.addContents('b', false);
+    // macroStructure['c'].insertionZone.addContents('c', false);
     macroStructure['imports'].insertionZone.addContents('imports', false);
     macroStructure['props'].insertionZone.addContents('props', false);
+    macroStructure['props'].insertionZone.contents[0][0].addContents('propsList', false);
     macroStructure['mocks'].insertionZone.addContents('mockVar', true);
-    macroStructure['jestHooks'].insertionZone.addContents('beforeAll', true);
-    macroStructure['jestHooks'].insertionZone.contents[0].contents[0].addContents('render', false)
-    macroStructure['props'].insertionZone.contents[0].contents[0].addContents('propsList', false);
+    macroStructure['jestHooks'].insertionZone.addContents('beforeAll', false)
+    macroStructure['jestHooks'].insertionZone.contents[0][0].addContents('render', true)    // macroStructure['props'].insertionZone.contents[0].contents[0].addContents('propsList', false);
     macroStructure['testZone'].insertionZone.addContents('testblock', true);
 
   }
@@ -250,6 +267,7 @@ const CreateTemplate = (props) => {
   const handleNewTest = (event) => {
 
     event.preventDefault();
+    console.log(macroStructure['testZone'].insertionZone);
     macroStructure['testZone'].insertionZone.addContents('testblock', true);
 
   }
@@ -258,11 +276,17 @@ const CreateTemplate = (props) => {
 
     event.preventDefault();
 
-    const zoneContents = macroStructure['testZone'].insertionZone.contents;
-    const mostRecent = zoneContents[zoneContents.length - 1];
 
-    doc.replaceRange('', {line: mostRecent.fetchPosition('s').line - 1, ch: 0}, {line: mostRecent.fetchPosition('e').line, ch: 0});
-    mostRecent.clearMarks();
+    const zoneContents = macroStructure['testZone'].insertionZone.insertionPts;
+    const mostRecent = zoneContents[zoneContents.length - 1]
+
+    const mostRecentStart = mostRecent.insertionStart.find();
+    const mostRecentEnd = mostRecent.insertionEnd.find();
+
+
+    doc.replaceRange('', {line: mostRecentStart.line - 1, ch: 0}, {line: mostRecentEnd.line, ch: 0});
+    mostRecent.insertionStart.clear();
+    mostRecent.insertionEnd.clear();
     zoneContents.pop();
 
   }
@@ -272,7 +296,13 @@ const CreateTemplate = (props) => {
     event.preventDefault();
 
     const insertionPt = selectorVal;
-    (macroStructure['testZone'].insertionZone.contents[selectorVal]).contents[0].addContents('expect', true);
+    macroStructure['testZone'].insertionZone.contents[selectorVal][0].addContents('expect', true);
+
+  }
+
+  const testButton = () => {
+
+    console.log(macroStructure['testZone'].insertionZone)
 
   }
 
@@ -295,7 +325,7 @@ const CreateTemplate = (props) => {
                 </div>
                 </div>
                   <form id="subsection3">
-                    <label> Undo, Redo or getAllMarks: </label>
+                    <label> Undo, Redo, getAllMarks, testButton: </label>
                     <br></br>
                     <br></br>
                     <button 
@@ -307,6 +337,9 @@ const CreateTemplate = (props) => {
                     <button 
                       onClick={() => { console.log(doc.getAllMarks()) }}
                     >getAllMarks</button>
+                    <button 
+                      onClick={testButton}
+                    >testButton</button>
                   </form>
                   <form id="subsection3">
                     <label> Add a New Test Block: </label>
@@ -326,12 +359,8 @@ const CreateTemplate = (props) => {
                     <input type="number" onChange={(event) => { event.preventDefault(); selectorVal = event.target.value; console.log(selectorVal) } } />
                     {/* <button type="submit" className="dropbtn" value="Add Expect" onClick={(event) => handleAssertions(event.target.value)} >Add Expect</button>
                     <button type="submit" className="dropbtn" value="Delete Expect" onClick={(event) => handleAssertions(event.target.value)} >Delete Expect</button> */}
-                    <button 
-                      type='submit' onClick={handleNewAssertion}
-                    >Add</button>
-                    <button 
-                      // onClick={handleNewTest}
-                    >Remove</button>
+                    <button type='submit' onClick={handleNewAssertion}>Add</button>
+                    <button>Remove</button>
                   </form>
               </div>
               <button className="dropbtn" id="bigSaveTemplateButton" onClick={goToFinalDraft}>Save Template</button>
@@ -356,4 +385,3 @@ const CreateTemplate = (props) => {
 }
 
 export default CreateTemplate;
-
