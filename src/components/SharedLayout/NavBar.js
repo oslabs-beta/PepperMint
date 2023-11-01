74cb7d8c16d33b09bd9961023ef954e0d5b5c8a0
Nassim Dev <nassimdeveloper@Nassims-MacBook-Pro.local>
2023-11-01 13:57:16 -0400

    Fixed some bugs in CreateTemplate

--- src/components/SharedLayout/NavBar.js
+++ src/components/SharedLayout/NavBar.js
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 function NavBar() {
     const navigate = useNavigate();
     const handle = (e) => {
+
+        e.preventDefault();
         navigate("/")
     }
 
